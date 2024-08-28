2b6dc17ba10ddd0bae67bf0e76aa6c0253864129
Anil G.R <anilgr.agr@gmail.com>
2024-08-28 16:09:48 +0530

    API Update.

--- src/new/playground.js
+++ src/new/playground.js
@@ -9,21 +9,23 @@ const { API } = require("./api");
 // parrayOf(...(PArray.deserialize("../../bk_data_4.txt").map(w=>w.trim()))).serialize("../../data_4.txt")
 // parrayOf(...(PArray.deserialize("../../bk_data_5.txt").map(w=>w.trim()))).serialize("../../data_5.txt")
 
+/* Group selected words into 3 4 and 5 letter words. */
 // const words = parrayOf(parrayOf(), parrayOf(), parrayOf())
 // PArray.deserialize(path.join(API_BASE_PATH, 'selected-words.txt')).forEach(w=>{
-//     const l = knTokenize(w).length
-//     words[l-3].push(w)
+    // const l = knTokenize(w).length
+    // words[l-3].push(w)
 // })
 // words.forEach(a=>a.push("-----------"))
 // parrayOf(...words.flat()).serialize(path.join(API_BASE_PATH, 'selected-words-grped.txt'))
 
+/* List words in api */
 // const words = parrayOf()
 // API.forEachRoute(({response})=>{
 //     words.push(response.solution)
 // })
 // words.serialize(path.join(API_BASE_PATH, "api-words.txt"))
 
-// insert five letter words in between the current selected words.
+/* insert five letter words in between the current selected words. */
 // const _5w = PArray.deserialize(rootFile('_5w.txt'));
 // const selected = PArray.deserialize(rootFile('selected-words-grped.txt')).randomize()
 // for(let i = 0; i < selected.length; i++) {
@@ -34,5 +36,11 @@ const { API } = require("./api");
 // _5w.serialize(rootFile('a.txt'))
 // selected.serialize(rootFile('b.txt'))
 
+/* Randomize selected words */
 const SELECTED_WORDS_PATH = path.join(API_BASE_PATH, 'selected-words.txt');
 PArray.deserialize(SELECTED_WORDS_PATH).randomize().serialize(SELECTED_WORDS_PATH);
+
+
+/* Export from Alar export json */
+// const bm = require('./alar_export.json').bookmarks;
+// PArray.from(bm.map(b=>b.word)).serialize("./exp-words.txt");
